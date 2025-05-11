8591231500f0c336cca3e2fc64e78190a20a3521
Hariharan-Murugan15 <Hariharan.Murugan@cognizant.com>
2025-05-11 10:21:52 +0530

    Add files via upload

---
+++ UserService-1/src/main/java/com/cognizant/UserService1Application.java
@@ -0,0 +1,17 @@
+package com.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserService1Application.class, args);
+	}
+
+}
